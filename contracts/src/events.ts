39af84a75a126b4665cbf705c60bf45488cefaa6
massa-pred-dev <massa-pred-dev@noreply.github.com>
2025-10-31 14:24:44 +0100

    feat: add event handling script to retrieve and log events from the Massa blockchain

---
+++ contracts/src/events.ts
@@ -0,0 +1,17 @@
+import { Account, Web3Provider } from '@massalabs/massa-web3';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const account = await Account.fromEnv('PRIVATE_KEY');
+const provider = Web3Provider.buildnet(account);
+
+const events = await provider.getEvents({
+  callerAddress: account.address.toString(),
+});
+
+for (const event of events) {
+  console.log('Event message:', event.data);
+}
+
+console.log('Done Evenst');
