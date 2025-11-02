416501e881a49cf984b70104a7ed88ab8f86919a
massa-pred-dev <massa-pred-dev@noreply.github.com>
2025-11-02 14:34:51 +0100

    refactor(contract): adjust deployment parameters for prediction market contract

--- contracts/src/deploy.ts
+++ contracts/src/deploy.ts
@@ -13,9 +13,12 @@ import { deployPredictionMarketContract } from './calls';
 const account = await Account.fromEnv();
 const provider = JsonRpcProvider.buildnet(account);
 
-
 // Intervals is 2 min, byffer is very large
-const contract = await deployPredictionMarketContract(provider, 120000, 300000000);
+const contract = await deployPredictionMarketContract(
+  provider,
+  90_000,
+  300000000,
+);
 
 const events = await provider.getEvents({
   smartContractAddress: contract.address,
